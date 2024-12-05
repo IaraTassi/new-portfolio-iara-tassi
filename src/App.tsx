cb5db697ee74411785c72c9413c409d99f38771b
IaraTassi <iaratassi@hotmail.com>
2024-12-05 13:41:24 -0300

    Adicionando Course.tsx. e Course.css e alguma dependências

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
+import Course from "./components/Course/Course";
+import "aos/dist/aos.css";
+import AOS from "aos";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+AOS.init({
+  duration: 1000,
+  once: true,
+});
 
 function App() {
   return (
     <div>
       <Navbar />
       <About />
+      <Course />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./components/Navbar/Navbar.css";
 import "./components/About/About.css";
+import "./components/Course/Course.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
